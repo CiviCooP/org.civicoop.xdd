1df42292cb60cd33f803c94b628504d1c49d2b4b
Alain Benbassat <alain@businessandcode.eu>
2016-07-21 12:38:50 +0200

    Added api

--- api/v3/Xdd/Run.php
+++ api/v3/Xdd/Run.php
@@ -1,40 +1,67 @@
 <?php
 
-/**
- * Xdd.Run API specification (optional)
- * This is used for documentation and validation.
- *
- * @param array $spec description of fields supported by this API call
- * @return void
- * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
- */
 function _civicrm_api3_xdd_Run_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
+  $spec['probability']['api.required'] = 1;
 }
 
-/**
- * Xdd.Run API
- *
- * @param array $params
- * @return array API result descriptor
- * @see civicrm_api3_create_success
- * @see civicrm_api3_create_error
- * @throws API_Exception
- */
 function civicrm_api3_xdd_Run($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array( // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
+  $validProbabilities = array('high', 'medium', 'low');
+  
+  if (array_key_exists('probability', $params) && in_array($params['probability'], $validProbabilities)) {
+    $returnValues = _civicrm_api3_xdd_RunDedupeRule($params['probability']);
+    
+    return civicrm_api3_create_success($returnValues, $params, 'xdd', 'run');
   } else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
+    // invalid parameter
+    $msg = ts("'probability' is a required parameter, and must be one of these values: ") . implode(',', $validProbabilities) . '. ' . ts("You specified: ") . $params['probability'];
+    throw new API_Exception($msg, 999);
   }
 }
 
+function _civicrm_api3_xdd_RunDedupeRule($probability) {
+  $xddOptionGroupID = _civicrm_api3_xdd_getOptionGroupID();
+  $xddDedupeRuleID = _civicrm_api3_xdd_getOption($xddOptionGroupID, 'dedupe_rule_' . $probability);
+  $xddDedupeGroupID = _civicrm_api3_xdd_getOption($xddOptionGroupID, 'dedupe_group_' . $probability);
+  
+  if ($xddOptionGroupID == 0 || $xddDedupeRuleID == 0 || $xddDedupeGroupID == 0) {
+    return ts('Nothing to do. Please check your XDD settings.');
+  }
+  
+  $dedupeRule = 
+  $dedupeGroup
+  // get the settings
+}
+
+private function _civicrm_api3_xdd_getOptionGroupID() {
+  // get the xdd option group
+  $params = array(
+    'name' => 'xdd_settings',
+    'sequential' => 1,
+  );
+  $xddOptionGroup = civicrm_api3('OptionGroup', 'get', $params);
+    
+  if ($xddOptionGroup['count'] == 0) {
+    return 0;
+  }
+  else {
+    $xddOptionGroup = reset($xddOptionGroup['values']);        
+    return $xddOptionGroup['id'];
+
+  }
+}
+
+private function _civicrm_api3_xdd_getOption($groupID, $valueName) {
+  $params = array(
+    'option_group_id' => $groupID,
+    'name' => $valueName,
+  );
+  $xddOptionValue = civicrm_api3('OptionValue', 'get', $params);
+    
+  if ($xddOptionValue['count'] == 0) {
+    return 0;
+  }
+  else {
+    $xddOptionValue = reset($xddOptionValue['values']);      
+    return $xddOptionValue['value'];
+  }
+}
